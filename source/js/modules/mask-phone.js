6104b961c2a54225bb9594e1027b96abd62cc8a7
Igor Rebrishchev <igorr28@yandex.ru>
2022-11-21 20:25:21 +0300

    добавляет правки по код-ревью

--- source/js/modules/mask-phone.js
+++ source/js/modules/mask-phone.js
@@ -1,21 +1,13 @@
 import IMask from 'imask';
 
-const tel = document.getElementById('tel');
-const telModal = document.getElementById('tel-modal');
-const maskOptions = {
-  mask: '+{7}(000)000-00-00',
-};
+const telInputs = Array.from(document.querySelectorAll('input[type="tel"]'));
 
 const initMask = () => {
-  if (tel) {
-    // eslint-disable-next-line no-unused-vars, new-cap
-    const mask = IMask(tel, maskOptions);
-  }
-
-  if (telModal) {
-    // eslint-disable-next-line no-unused-vars, new-cap
-    const mask2 = IMask(telModal, maskOptions);
-  }
+  telInputs.forEach(
+      (telInput) => new IMask(telInput, {
+        mask: '+{7}(000)000-00-00',
+      })
+  );
 };
 
 export {initMask};
